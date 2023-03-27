6266149725dccf8421690619567c8cc349b46142
FaroukOyekunle <oyekunlefarouk2001@gmail.com>
2023-03-27 06:57:33 -0700

    main - check the code Return: Always 0.

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "Love for all hatred for none";
+    print_rev(str);
+    return (0);
+}
