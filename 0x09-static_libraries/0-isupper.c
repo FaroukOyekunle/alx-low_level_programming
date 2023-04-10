9ff97c01d5fbb3757df8bd47a2962a7ca2af79af
FaroukOyekunle <oyekunlefarouk2001@gmail.com>
2023-04-10 14:20:15 -0700

    added main.c file

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks for uppercase character
+ *
+ * Description: function that checks for uppercase character
+ *
+ * @c: character to check
+ *
+ * Return: 1 if uppercase and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
