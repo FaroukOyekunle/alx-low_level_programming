4ea21a21f76cf56706f7867982a5d09e0de44690
FaroukOyekunle <oyekunlefarouk2001@gmail.com>
2023-04-10 14:54:21 -0700

    added main.c file

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _abs - Program prints the lowercase alphabet
+*
+* Description: Program prints the lower case alphabet using
+* _puthchar and a for loop over the ascii table character set
+*
+* @n: ASCII equivalent integer
+*
+* Return: 1 if lowercase and 0 otherwise
+*
+*/
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
