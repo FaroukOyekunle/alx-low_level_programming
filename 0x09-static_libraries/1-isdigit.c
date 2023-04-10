2360361268a85f2b9d6962670781c3b8a3de0a49
FaroukOyekunle <oyekunlefarouk2001@gmail.com>
2023-04-10 14:26:05 -0700

    added main.c file

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that checks for a digit
+ *
+ * Description: function that checks for a digit
+ *
+ * @c: character to check
+ *
+ * Return: 1 if digit and 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
