8626659bd4d05bcb4099892926406333f885cb66
FaroukOyekunle <oyekunlefarouk2001@gmail.com>
2023-07-08 05:59:12 -0700

    Write a function that returns the value of a bit at a given index.  Prototype: int get_bit(unsigned long int n, unsigned int index);

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of a number
+ *
+ * @n: The number to be printed in binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1;
+	int bits = sizeof(unsigned long int) * 8;
+	int flag = 0;
+
+	mask <<= (bits - 1); /* Move the 1 to the most significant bit */
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (mask)
+	{
+		if ((n & mask) == 0)
+		{
+			if (flag)
+				_putchar('0');
+		}
+		else
+		{
+			_putchar('1');
+			flag = 1;
+		}
+
+		mask >>= 1;
+	}
+
+}
