0d75d6eca162b1bf36344a340ef873f4d431c190
FaroukOyekunle <oyekunlefarouk2001@gmail.com>
2023-07-08 04:39:24 -0700

    Write a function that converts a binary number to an unsigned int.   Prototype: unsigned int binary_to_uint(const char *b);

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,43 +1,46 @@
-s a string
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary to an unsigned int
+ * @b: the binary number as a string
  *
- *  * Return: The converted value
- *   */
- unsigned int binary_to_uint(const char *b)
+ * Return: the converted value
+ */
+unsigned int binary_to_uint(const char *b)
 {
-		unsigned int decimal = 0;
-			int str_len = 0, base = 1;
+unsigned int decimal = 0;
+int str_len = 0, base = 1;
 
-				if (!check_valid_string(b))
-							return (0);
+if (!check_valid_string(b))
+return (0);
 
-					while (b[str_len] != '\0')
-								str_len++;
+while (b[str_len] != '\0')
+str_len++;
 
-						while (str_len)
-								{
-											decimal += ((b[str_len - 1] - '0') * base);
-													base *= 2;
-															str_len--;
-																}
-							return (decimal);
+while (str_len)
+{
+decimal += ((b[str_len - 1] - '0') * base);
+base *= 2;
+str_len--;
+}
+return (decimal);
 }
 
 /**
- *  * check_valid_string - Checks if a string has only 0's and 1's
- *   * @b: String to be checked
- *    *
- *     * Return: 1 if the string is valid, 0 otherwise
- *      */
+ * check_valid_string - checks if a string has only 0's and 1's
+ * @b: string to be checked
+ *
+ * Return: 1 if string is valid, 0 otherwise
+ */
 int check_valid_string(const char *b)
 {
-		if (b == NULL)
-					return (0);
+if (b == NULL)
+return (0);
 
-			while (*b)
-					{
-								if (*b != '1' && *b != '0')
-												return (0);
-										b++;
-											}
-				return (1);
+while (*b)
+{
+if (*b != '1' && *b != '0')
+return (0);
+b++;
+}
+return (1);
 }
