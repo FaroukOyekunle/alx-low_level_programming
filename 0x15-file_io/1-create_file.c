162649f0e259ec24e323d6a2c0d3c29acda5327b
FaroukOyekunle <oyekunlefarouk2001@gmail.com>
2023-07-10 07:59:58 -0700

    Create a function that creates a file.  Prototype: int create_file(const char *filename, char *text_content);

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,23 +12,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, bytes_written = 0;
+	ssize_t len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; len++)
+			;
 
-	ssize_t len = strlen(text_content);
-	ssize_t bytes_written = write(fd, text_content, len);
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
 	close(fd);
-
-	return (bytes_written == len ? 1 : -1);
+	return (1);
 }
 
