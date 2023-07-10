0ea286f3ac523b211ae76f79610d6df62ecc4b60
FaroukOyekunle <oyekunlefarouk2001@gmail.com>
2023-07-10 07:31:01 -0700

    Create a function that creates a file.  Prototype: int create_file(const char *filename, char *text_content);

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,30 +1,36 @@
 #include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * create_file - function that creates a file
  *
- * Return: If the function fails - -1.
- * Otherwise - 1.
+ * Description: function that creates a file
+ *
+ * @filename: filename
+ * @text_content: text
+ *
+ * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content) {
+    int fd;
+
     if (filename == NULL)
         return -1;
 
-    int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
     if (fd == -1)
         return -1;
 
-    if (text_content != NULL) {
-        ssize_t bytesWritten = write(fd, text_content, strlen(text_content));
-        if (bytesWritten == -1) {
-            close(fd);
-            return -1;
-        }
-    }
+    if (text_content == NULL)
+        text_content = "";
+
+    ssize_t len = strlen(text_content);
+    ssize_t bytesWritten = write(fd, text_content, len);
 
     close(fd);
-    return 1;
+
+    return (bytesWritten == len) ? 1 : -1;
 }
