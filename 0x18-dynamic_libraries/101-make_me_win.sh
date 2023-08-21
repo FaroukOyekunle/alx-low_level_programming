a6f4fba760e4c16908b7a4d16e2f61b715bbdcdc
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-08-21 13:08:52 +0100

    Delete 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/FaroukOyekunle/alx-low_level_programming/master/0x18-dynamic_libraries/librand.so -O ../101-make_me_win.so
-export LD_PRELOAD="../101-make_me_win.so"
